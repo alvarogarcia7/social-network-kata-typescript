6a388f540ca1d2789de895aa1da17714f69b9c5f
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-14 03:33:54 +0400

    This commit was red
    Revert "green: assert on the saved message"
    
    This reverts commit 938c278b342797d2d75f8216bcc4bfe77a4852d2.

--- src/PublishMessage.test.ts
+++ src/PublishMessage.test.ts
@@ -22,9 +22,6 @@ describe('Publish Message', () => {
         const answer = publishMessage.publish(new Message(new User('Alice'), 'a message'))
 
         expect(messageRepository.save).toHaveBeenCalled()
-        expect(messageRepository.load(new User('Alice'))).toEqual([
-            new Message(new User('Alice'), 'a message')
-        ])
         expect(answer).toBeTruthy()
     })
 
